a475fc9313cbe184682f8607a1af056025d483ae
sakuraba <sakuraba@fresco.co.jp>
2023-09-28 15:32:58 +0900

    header と footer の自動読み込み化

--- js/main.js
+++ js/main.js
@@ -10,3 +10,45 @@ function header(rootDir) {
 		}
 	});
 }
+
+function include_header(rootDir) {
+	$.ajax({
+		url: rootDir + 'include/header.html', // リクエストを送信するURLを指定
+		async: false, // 非同期リクエストを無効にする
+	}).done(function (header_html) { // 通信が成功したら
+		header_html = header_html.replace(/\{root\}/g, rootDir); // header.htmlの文字列を置き換え
+
+		document.write(header_html); // herder.htmlを表示する
+	});
+}
+
+function include_footer(rootDir) {
+	var footerString = '<footer>' +
+		'<div style="text-align: right;">' +
+		'Mastodon : <a href="https://mstdn.shihoya.tokyo/@ARTi">@ARTi@mstdn.shihoya.tokyo</a>' +
+		'</div>' +
+		'</footer>';
+	document.write(footerString);
+}
+
+function header_with_date(datetime, rootDir) {
+	var year = datetime.getFullYear();
+	var month = datetime.getMonth() + 1;
+	var day = datetime.getDate();
+	var dayOfWeek = datetime.getDay();
+	var dayOfWeekStr = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][dayOfWeek];
+	var dateString = year + '/' + month + '/' + day + ' ' + dayOfWeekStr + '.';
+
+	var headerSource = '<header>' +
+		'<div style = "text-align: right;">' +
+		'<time datetime=' + datetime + '>' + dateString + '</time>' +
+		'<br />' +
+		'Mastodon: <a href="https://mstdn.shihoya.tokyo/@ARTi">@ARTi@mstdn.shihoya.tokyo</a>' +
+		'</div>' +
+		'</header>';
+	document.write(headerSource);
+}
+
+function helloWorld() {
+	document.write("AAAAAA");
+}
